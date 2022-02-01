e460e836c49ca8d32abdb675717d13fdfe08a611
Garv Jain <garvj03@gmail.com>
2022-02-01 13:43:57 +0530

    ui: fix padding

--- app/routes/alumni.tsx
+++ app/routes/alumni.tsx
@@ -1,53 +1,63 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import useTitle from "~/hooks/use-title";
 import Header from "~/components/Header";
-import '~/data/alumni'
-import {alumni} from "~/data/alumni";
+import "~/data/alumni";
+import { alumni } from "~/data/alumni";
 
 const AlumniPage = () => {
-    useTitle("Alumni");
+  useTitle("Alumni");
 
-    const [search, setSearch] = useState("");
+  const [search, setSearch] = useState("");
 
-    const alum = alumni.map((alum) => {
-        return <div>
-            <h2 className={"text-black text-3xl pb-6 pt-12"}>Batch of {alum.batch}</h2>
-            <div className="flex flex-wrap -m-2">
-                {alum.people.map((mem) => {
-                    return <div className="p-2 lg:w-1/5 md:w-1/2 w-full">
-                        <div className="h-32 flex items-center border-black border-2 p-4">
-                            <div className="flex-grow p-5">
-                                <h2 className="text-gray-900 title-font font-medium text-xl">{mem.name}</h2>
-                                <p className="text-gray-500">{mem.role}</p>
-                            </div>
-                        </div>
-                    </div>
-                })}
-            </div>
-        </div>
-    });
-
+  const alum = alumni.map((alum) => {
     return (
-        <div>
-            <Header/>
-            <section className="text-gray-600 body-font">
-                <div className="container px-5 py-24 mx-auto">
-                    <div className="flex flex-col w-full">
-                        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-1 text-gray-900">Alumni</h1>
-                        <p className="lg:w-2/3 leading-relaxed text-base mb-8">Roboknights has an extensive, well connected
-                            alumni network which guides and mentors its members into the right path.</p>
-                        <input
-                            id={"search"}
-                            type="text"
-                            className="border-2 p-2 border-black w-60"
-                            placeholder="Search"
-                        />
-                    </div>
-                    {alum}
+      <div>
+        <h2 className={"text-black text-3xl pb-6 pt-12"}>
+          Batch of {alum.batch}
+        </h2>
+        <div className="flex flex-wrap -m-2">
+          {alum.people.map((mem) => {
+            return (
+              <div className="p-2 lg:w-1/5 md:w-1/2 w-full">
+                <div className="h-32 flex items-center border-black border-2 p-4">
+                  <div className="flex-grow p-5">
+                    <h2 className="text-gray-900 title-font font-medium text-xl">
+                      {mem.name}
+                    </h2>
+                    <p className="text-gray-500">{mem.role}</p>
+                  </div>
                 </div>
-            </section>
+              </div>
+            );
+          })}
         </div>
+      </div>
     );
+  });
+
+  return (
+    <div>
+      <Header />
+      <div className="container mx-auto md:px-24 px-5 py-12">
+        <div className="flex flex-col w-full">
+          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-1 text-gray-900">
+            Alumni
+          </h1>
+          <p className="lg:w-2/3 leading-relaxed text-base mb-8">
+            Roboknights has an extensive, well connected alumni network which
+            guides and mentors its members into the right path.
+          </p>
+          <input
+            id={"search"}
+            type="text"
+            className="border-2 p-2 border-black w-60"
+            placeholder="Search"
+          />
+        </div>
+        {alum}
+      </div>
+    </div>
+  );
 };
 
 export default AlumniPage;
--- app/routes/members.tsx
+++ app/routes/members.tsx
@@ -7,14 +7,23 @@ const MembersPage = () => {
   return (
     <div>
       <Header />
-      <section>
-        <h1>Members</h1>
-        <input
-          type="text"
-          className="border-2 p-2 border-black "
-          placeholder="Search"
-        />
-      </section>
+      <div className="container px-5 py-12 mx-auto">
+        <div className="flex flex-col w-full">
+          <h1 className="sm:text-3xl text-2xl font-bold title-font mb-1 text-gray-900">
+            Members
+          </h1>
+          <p className="lg:w-2/3 leading-relaxed text-base mb-8">
+            Roboknights has an extensive, well connected alumni network which
+            guides and mentors its members into the right path.
+          </p>
+          <input
+            id={"search"}
+            type="text"
+            className="border-2 p-2 border-black w-60"
+            placeholder="Search Members"
+          />
+        </div>
+      </div>
     </div>
   );
 };
