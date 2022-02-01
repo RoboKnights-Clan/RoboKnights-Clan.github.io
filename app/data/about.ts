448cb2596c548192c5e74a97b71c5339b8e8742c
Garv Jain <garvj03@gmail.com>
2022-02-01 13:44:32 +0530

    chore: prepare data

---
+++ app/data/about.ts
@@ -0,0 +1,20 @@
+export const about = [
+  {
+    name: "Ms. Padma Shrinivasan",
+    role: "Principal, DPS RK Puram",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Euismod lectus vulputate proin volutpat, nunc purus. Vehicula ultrices velit et diam lacus accumsan. Mauris enim orci diam egestas viverra consequat. Enim pellentesque consectetur placerat felis quam. Lorem ultrices sit et sed est. Nisl eget venenatis integer dolor, senectus lectus. Sagittis felis in porttitor tempus euismod lacus. Imperdiet sit iaculis feugiat eget tempus dui hac turpis.",
+    image: "/images/principal.png",
+  },
+  {
+    name: "Mr. KG Ajithkumar",
+    role: "Club Incharge, Roboknights",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Euismod lectus vulputate proin volutpat, nunc purus. Vehicula ultrices velit et diam lacus accumsan. Mauris enim orci diam egestas viverra consequat. Enim pellentesque consectetur placerat felis quam. Lorem ultrices sit et sed est. Nisl eget venenatis integer dolor, senectus lectus. Sagittis felis in porttitor tempus euismod lacus. Imperdiet sit iaculis feugiat eget tempus dui hac turpis.",
+    image: "/images/incharge.png",
+  },
+  {
+    name: "Mr. Mukesh Kumar",
+    role: "Supervisor and Head of Department, Computer Science",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Euismod lectus vulputate proin volutpat, nunc purus. Vehicula ultrices velit et diam lacus accumsan. Mauris enim orci diam egestas viverra consequat. Enim pellentesque consectetur placerat felis quam. Lorem ultrices sit et sed est. Nisl eget venenatis integer dolor, senectus lectus. Sagittis felis in porttitor tempus euismod lacus. Imperdiet sit iaculis feugiat eget tempus dui hac turpis.",
+    image: "/images/mukesh_sir.png",
+  },
+];
--- app/data/members.ts
+++ app/data/members.ts
@@ -1,174 +1,59 @@
-const members = [
-  {
-    year: "Class 12",
-    members: [
-      {
-        name: "Advitya Singhal",
-        position: "President",
-        image: "aditya.jpg",
-      },
-      {
-        name: "Kshitij Gupta",
-        position: "President",
-        image: "kshitij.jpg",
-      },
-      {
-        name: "Yash Jha",
-        position: "Vice President",
-        image: "yash.jpg",
-      },
-      {
-        name: "Tanmay Singhal",
-        position: "Vice President",
-        image: "tanmay.jpg",
-      },
-      {
-        name: "Ayaan Kapur",
-        position: "Director",
-        image: "ayaan.jpg",
-      },
-      {
-        name: "Shashwat Raj",
-        position: "Senior Member",
-        image: "shashwat.jpg",
-      },
-      {
-        name: "Aniket Negi",
-        position: "Senior Member",
-        image: "aniket.jpg",
-      },
-    ],
+export const members = [
+  { "year": "Class 12",
+    "members": [
+      { "name": "Kshitij Gupta", "role": "President", "src":"https://media.discordapp.net/attachments/828640298354737163/828842237978476544/Screenshot_2021-04-06_092745.png" },
+      { "name": "Advitya Singhal", "role": "President", "src":"https://cdn.discordapp.com/attachments/753327283497992356/837886153087909938/IMG_20210501_082959.jpg" },
+      { "name": "Yash Jha", "role": "Vice President", "src":"https://cdn.discordapp.com/attachments/753327283497992356/837785029881888798/Screenshot_20210501-014813.jpg" },
+      { "name": "Tanmay Singhal", "role": "Vice President", "src":"https://media.discordapp.net/attachments/753327283497992356/893406736856272896/tanmaypsc.png" },
+      { "name": "Ayaan Kapur", "role": "Director", "src":"https://cdn.discordapp.com/attachments/753327283497992356/837939071169855548/IMG-20201112-WA0006.jpg" },
+      { "name": "Aniket Negi", "role": "Senior Member" ,"src":"https://cdn.discordapp.com/attachments/753327283497992356/837564111771926608/xD.png"},
+      { "name": "Shashwat Raj", "role": "Senior Member", "src":"https://media.discordapp.net/attachments/828640298354737163/828842232898650152/shashwat.jpg" },
+      { "name": "Rudra Pruthi", "role": "Member", "src":"https://cdn.discordapp.com/attachments/753327283497992356/838303745753350144/IMG-20210502-WA0006__01.jpg" }
+    ]
   },
   {
-    year: "Class 11",
-    members: [
-      {
-        name: "Salil Mittal",
-        position: "Senior Member",
-        image: "salil.jpg",
-      },
-      {
-        name: "Ayush Daga",
-        position: "Senior Member",
-        image: "ayush.jpg",
-      },
-      {
-        name: "Anubhav Choudhery",
-        position: "Senior Member",
-        image: "anubhav.jpg",
-      },
-      {
-        name: "Yashas Gogna",
-        position: "Senior Member",
-        image: "yashas.jpg",
-      },
-      {
-        name: "Nikunj Agarwal",
-        position: "Senior Member",
-        image: "nikunj.jpg",
-      },
-      {
-        name: "Ishaan Attam",
-        position: "Senior Member",
-        image: "ishaan.jpg",
-      },
-      {
-        name: "Pratye Agarwal",
-        position: "Senior Member",
-        image: "pratye.jpg",
-      },
-    ],
+    "year": "Class 11",
+    "members": [ 
+      { "name": "Salil Mittal", "role": "Senior Member", "src":"https://media.discordapp.net/attachments/753327283497992356/884071755910615090/profile__.jpg?width=670&height=670" },
+      { "name": "Ishaan Attam", "role": "Senior Member", "src":"https://media.discordapp.net/attachments/883993840149753906/883998407440629780/IMG-20210905-WA0004.jpg?width=670&height=670" },
+      { "name": "Pratye Aggarwal", "role": "Senior Member", "src":"https://media.discordapp.net/attachments/753327283497992356/884022120701653012/Pratye_Aggarwal.jpg?width=670&height=670" },
+      { "name": "Ayush Daga", "role": "Senior Member", "src":"https://media.discordapp.net/attachments/888667530959417345/892729445000020028/IMG_3954-1.jpg?width=670&height=670" },
+      { "name": "Nikunj Aggarwal", "role": "Member", "src":"https://media.discordapp.net/attachments/893514358141902858/893514451423203359/Nikunj_Agarwal_11I.jpeg?width=670&height=670" },
+      { "name": "Anubhav Choudhery", "role": "Member", "src":"https://media.discordapp.net/attachments/828640298354737163/828842225973723206/Anubhav.jpg?width=1162&height=1135" },
+      { "name": "Yashas Gogna", "role": "Member", "src":"https://cdn.discordapp.com/attachments/753327283497992356/837730421264351392/Screenshot_20210430-220523__01.jpg"}
+    ]
   },
   {
-    year: "Class 10",
-    members: [
-      {
-        name: "Garv Jain",
-        position: "Senior Member",
-        image: "garv.jpg",
-      },
-      {
-        name: "Tanishq Gogia",
-        position: "Senior Member",
-        image: "tanishq.jpg",
-      },
-      {
-        name: "Adi Mathur",
-        position: "Senior Member",
-        image: "adi.jpg",
-      },
-      {
-        name: "Swarnika Bharadwaj",
-        position: "Senior Member",
-        image: "swarnika.jpg",
-      },
-      {
-        name: "Bhavya Verma",
-        position: "Senior Member",
-        image: "bhavya.jpg",
-      },
-      {
-        name: "Kushagra Goel",
-        position: "Senior Member",
-        image: "kushagra.jpg",
-      },
-      {
-        name: "Jasnoor Kaur",
-        position: "Senior Member",
-        image: "jasnoor.jpg",
-      },
-      {
-        name: "Rishit Yadav",
-        position: "Member",
-        image: "rishit.jpg",
-      },
-    ],
+    "year": "Class 10",
+    "members": [
+      { "name": "Tanishq Gogia", "role": "Senior Member", "src":"https://cdn.discordapp.com/attachments/753327283497992356/837967552293502996/Tanishq.jpg" },
+      { "name": "Kushagra Goel", "role": "Senior Member", "src":"https://media.discordapp.net/attachments/839774595463774208/929955176247361536/6b32fe76-3a6b-42fb-a1b4-c2f0f1f7a484.png?width=503&height=670" },
+      { "name": "Garv Jain", "role": "Senior Member", "src":"https://media.discordapp.net/attachments/869407124223053868/932306637467824209/Garv.jpg?width=616&height=670" },
+      { "name": "Bhavya Verma", "role": "Senior Member", "src":"https://media.discordapp.net/attachments/762628251977777152/894875697682739231/unknown.png" },
+      { "name": "Jasnoor Kaur", "role": "Senior Member", "src":"https://media.discordapp.net/attachments/828640298354737163/828842231086579742/Jasnoor.png" }, 
+      { "name": "Swarnika Bhardwaj", "role": "Senior Member", "src":"https://media.discordapp.net/attachments/869407124223053868/893422833886175242/IMG-20210929-WA0012.jpg" },
+      { "name": "Adi Mathur", "role": "Senior Member", "src":"https://media.discordapp.net/attachments/876057968771235860/893402396078772224/rk_photo.png?width=591&height=670" },
+      { "name": "Rishit Yadav", "role": "Member", "src":"https://cdn.discordapp.com/attachments/754714308604067841/838470434671558726/FINAL.png" }
+    ]
   },
   {
-    year: "Class 9",
-    members: [
-      {
-        name: "S. Shrikirti",
-        position: "Member",
-        image: "shrikirti.jpg",
-      },
-      {
-        name: "Rihit Wadhwa",
-        position: "Member",
-        image: "rihit.jpg",
-      },
-    ],
+    "year": "Class 9",
+    "members": [
+      { "name": "S. Shrikirti", "role": "Member" ,"src":"https://media.discordapp.net/attachments/882320970952613908/892626566142623744/01ecb3da-0a42-438d-893d-05d96aa5cc8b.png?width=670&height=670"},
+      { "name": "Rihit Wadhwa", "role": "Member", "src":"https://cdn.discordapp.com/attachments/753327283497992356/837783115946262568/Screenshot_20210501-013823__01.jpg" }
+    ]
   },
   {
-    year: "Class 8",
-    members: [
-      {
-        name: "Yuvraj Matta",
-        position: "Member",
-        image: "yuvraj.jpg",
-      },
-      {
-        name: "Shaurya Markanda",
-        position: "Member",
-        image: "shaurya.jpg",
-      },
-    ],
+    "year": "Class 8",
+    "members": [
+      { "name": "Yuvraj Matta", "role": "Member", "src":"https://cdn.discordapp.com/attachments/828596414702420031/874497624911278090/image0.png" },
+      { "name": "Shaurya Markanda", "role": "Member","src":"https://cdn.discordapp.com/attachments/828640298354737163/832498311259750410/New_Project.png" }
+    ]
   },
   {
-    year: "Class 7",
-    members: [
-      {
-        name: "Neil Sindwani",
-        position: "Member",
-        image: "neil.jpg",
-      },
-      {
-        name: "Agatsya Vatts",
-        position: "Member",
-        image: "agatsya.jpg",
-      },
-    ],
-  },
-];
-
-export default members;
+    "year": "Class 7",
+    "members": [
+      { "name": "Neil Sindwani", "role": "Member", "src":"https://media.discordapp.net/attachments/893463246252691467/893463836802301952/WhatsApp_Image_2021-10-01_at_17.11.11.jpeg?width=616&height=669" }
+    ]
+  }
+]
