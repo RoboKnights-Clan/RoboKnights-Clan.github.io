1fc7660f8a35de790a8d9bede6891ad66df1da0b
Garv Jain <garvj03@gmail.com>
2022-02-08 10:38:47 +0530

    feat: Resources Page

---
+++ data/resources.ts
@@ -0,0 +1,56 @@
+export const resources = [
+  {
+    name: "Physics and Maths Prerequisites",
+    date: "May 6, 2021",
+    desc: "Mathematics and Physics are the prime requirements for making something before you actually make it! In other words, they help you model the whole machine in your brain and on paper, and even simulate it, without actually making it.",
+    link: "https://docs.google.com/document/d/e/2PACX-1vRP8TvoR8bA1Cbhc3CXm_oihUCSWkUA2A6LHFAnTElHLJ7RQt7j3GhV8NXIhS9IV81f56bAfch9Y5DS/pub",
+  },
+  {
+    name: "Basic Electronics",
+    date: "May 6, 2021",
+    desc: "To say that the world of electronics is a core part of our lives is probably an understatement. The device you’re reading this resource on, your TV, your Air Conditioner… the list just keeps going on and on…. electronics is probably the single most important thing that’s powering these devices.",
+    link: "https://docs.google.com/document/d/e/2PACX-1vQw6c0dRLHfkFtcbXYRYYH3yxEZK_2_BDPmvMvCNl4GGfSr2JMLCAY6eY-mbFdXUUOBCTo1fAfzDYUr/pub",
+  },
+  {
+    name: "Mechanical Machines and Structures",
+    date: "May 6, 2021",
+    desc: "All ‘machines’ you see daily come into different categories: some are purely mechanical like a swing, some are purely electrical like your mobile phone, and some are even purely optical - like your spectacles. However, most machines we’ll deal with are an effective combination of the first two parts - mechanics and electricity.",
+    link: "https://docs.google.com/document/d/e/2PACX-1vQR3FL_RA9HQlj3adZ71SyJQTEgLpjN653gTEGlNLPsaPsyOUgIbSafxJYP9KH_u92gCQTQSPZNzDgo/pub",
+  },
+  {
+    name: "Introduction to Arduino",
+    date: "May 6, 2021",
+    desc: "Arduino refers to a family of development boards designed by USA based company Arduino, using microcontrollers manufactured mainly by Atmel. The company was founded and originally based in Italy but is now headquartered in the USA.",
+    link: "https://docs.google.com/document/d/e/2PACX-1vSUUmk39yOWc_M7OW4g-CDIb7a9rfiIVbrQFylwVLBU9v3XGph59Wy4yB8zBo5jF7UVz2kRpvvDmn9s/pub",
+  },
+  {
+    name: "Introduction to Sensors",
+    date: "May 6, 2021",
+    desc: "In layman’s terms, a sensor is an input device which provides a signal to a microcontroller that can be converted to a specific physical quantity. But what does this really mean?",
+    link: "https://docs.google.com/document/d/e/2PACX-1vSA7vLaR7eA2lFY1Uz391JW__XAjVfGdknGaYb_DbATsx9LNwdhvzd7SJ0hc6Jcw06RiVuKIYjoPjmB/pub",
+  },
+  {
+    name: "Internet of Things",
+    date: "July 15, 2021",
+    desc: "In a nutshell, it is the exchange of information got through the connections between sensors and other technology over the internet. From your car to a refrigerator, from a cell phone to mp3 players, most of the digital devices we see are connected to an IoT network.",
+    link: "https://docs.google.com/document/d/e/2PACX-1vRVFj2ovCNcHTWCpMQ7abFeDj1q2I0wT3smP572v5pt0lfTmuNgPDBfy4cGaPJ_krMCXWFXEif3n4Gl/pub",
+  },
+  {
+    name: "Advanced Programming— Dijkstra's Algorithm",
+    date: "May 6, 2021",
+    desc: "Mobile robots have a wide range of applications. Path-planning is an important primitive for autonomous mobile robots that lets robots find the ‘shortest’, or the ‘otherwise optimal’ path between two points.",
+    link: "https://docs.google.com/document/d/e/2PACX-1vSepKJHQE3Zm_eL20kLEHYFjOZD1ED7CRlZwerTnyEkBeARbQ0Rw9KF-OPrfjzKv3Rb8_9zvcr0M7df/pub",
+  },
+  {
+    name: "Advanced Programming— Linear Regression",
+    date: "May 6, 2021",
+    desc: "Machine Learning is everywhere. This is a phrase we see often these days, and it’s pretty close to a genuine truism. Netflix, Amazon, Siri, Pandora, and the list goes on. But it’s not just entertainment and media.",
+    link: "https://docs.google.com/document/d/e/2PACX-1vSaHk7u5ECByuOTD-ciZ2oJXNT_dxTAUN6WGMdUbAZFSX3IjkymJ9db0VGPITC_rnIbIFc2ex1yPODw/pub",
+  },
+  {
+    name: "Advanced Programming— PID Control Algorithm",
+    date: "May 6, 2021",
+    desc: "PID stands for Proportional - Integral - Derivative. Now, don’t panic. While the title screams calculus (and at the lowest level it is calculus), in practice, you don’t need an in-depth understanding of the math to implement it.",
+    link: "https://docs.google.com/document/d/e/2PACX-1vRB-VmYHpyutXp27sr-yiTKkT_TOKEBc0lJvz23G1N_krG3V-t9QHYzoB0XEZzJ3dt7QgKFffik2shy/pub",
+  },
+];
---
+++ pages/resources.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Header from "../components/Header";
+import { resources } from "../data/resources";
+import useTitle from "../hooks/use-title";
+
+const ResourcesPage = () => {
+  useTitle("Resources");
+  return (
+    <div>
+      <Header />
+      <div className="container mx-auto md:px-24 px-5 py-8">
+        <div className="flex flex-col w-full mb-8">
+          <h1 className="sm:text-3xl text-2xl font-bold title-font mb-2 text-gray-900">
+            Resources
+          </h1>
+          <p className="lg:w-2/3 leading-relaxed text-base mb-4">
+            We at RoboKnights not only focus on accolades and competitions but
+            also thrive to help our fellow members and other enthusiasts out
+            there to increase their knowledge in the growing field of robotics.
+            For this sole purpose, we have made the following resources to
+            enrich any and all interested.
+          </p>
+        </div>
+        <div className="flex flex-col">
+          {resources.map((resource, index) => (
+            <div className="border-2 border-black p-5 mb-4" key={index}>
+              <h1 className="title-font sm:text-2xl text-xl mb-2 font-semibold text-gray-900">
+                {resource.name}
+              </h1>
+              <h2 className="mb-2 text-lg">{resource.date}</h2>
+              <p className="leading-relaxed text-base mb-2">{resource.desc}</p>
+              <a href={resource.link} className="underline text-lg">
+                Read More
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ResourcesPage;
