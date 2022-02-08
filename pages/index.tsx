74a96584a614acbc4dd4e3bc0638ff6ad83f9c2c
Garv Jain <garvj03@gmail.com>
2022-02-08 09:52:08 +0530

    Added page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,10 @@ import type { NextPage } from "next";
 import Header from "../components/Header";
 import Hero from "../components/Home/Hero";
 import Sponsors from "../components/Home/Sponsors";
+import useTitle from "../hooks/use-title";
 
 const Home: NextPage = () => {
+  useTitle("Home");
   return (
     <div>
       <Header />
