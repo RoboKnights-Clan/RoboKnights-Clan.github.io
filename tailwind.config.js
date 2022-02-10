c0520e03d89dfb884e6a6d16852b1c942566ce77
Garv Jain <garvj03@gmail.com>
2022-02-10 15:08:07 +0530

    chore: configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#242424",
+      },
+    },
   },
   plugins: [],
 };
