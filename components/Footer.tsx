9e73913594dd5cc6c32087d3f2833f03269957ff
Garv Jain <garvj03@gmail.com>
2022-02-10 15:08:31 +0530

    Added socials

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,31 @@
 import React from "react";
 
+const socials = [
+  {
+    name: "facebook",
+    link: "https://www.facebook.com/roboknightsdps",
+    src: "/icons/facebook.png",
+  },
+  {
+    name: "instagram",
+    link: "https://www.instagram.com/roboknights_dpsrkp/",
+    src: "/icons/instagram.png",
+  },
+  {
+    name: "github",
+    link: "https://github.com/RoboKnights-Clan/",
+    src: "/icons/github.png",
+  },
+  {
+    name: "linkedin",
+    link: "https://www.linkedin.com/company/roboknights/mycompany/",
+    src: "/icons/linkedin.png",
+  },
+];
+
 const Footer = () => {
   return (
-    <div className="container px-5 py-12 mx-auto md:px-24 flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap bg-black flex-col">
+    <div className="container px-5 py-12 flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap bg-dark flex-col">
       <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
         <a className="flex title-font font-medium items-center md:justify-start justify-center py-12 text-gray-900">
           <img src="/images/traced_logo.png" className="w-full" alt="ok" />
@@ -22,7 +45,18 @@ const Footer = () => {
           <h2 className="title-font font-bold text-white tracking-widest text-lg mb-3">
             Our Socials
           </h2>
-          <div className="flex flew-row"></div>
+          <div className="flex flew-row text-white">
+            {socials.map((social) => (
+              <a
+                target="_blank"
+                key={social.name}
+                href={social.link}
+                className="text-white hover:text-gray-300 mr-2 flex items-center"
+              >
+                <img src={social.src} className="w-7 h-7" alt={social.name} />
+              </a>
+            ))}
+          </div>
         </div>
         <div className="lg:w-1/2 md:w-1/2 w-full px-4">
           <h2 className="title-font font-bold text-white tracking-widest text-lg mb-3">
