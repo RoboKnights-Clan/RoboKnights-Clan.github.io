64dc01b169cc97352f599cf8194ddac82743a8d4
Garv Jain <garvj03@gmail.com>
2022-02-17 10:07:51 +0530

    feat: achievements data for 2021

--- data/achievements.ts
+++ data/achievements.ts
@@ -1,14 +1,99 @@
 export const achievements = [
-    {
-        competition: "Test",
-        level: "National",
-        year: "2019",
-        prize: "Winners"
-    },
-    {
-        competition: "Test",
-        level: "National",
-        year: "2019",
-        prize: "Winners"
-    }
-]
+  {
+    competition:
+      "Robotics event of Silico Battles v17.1, organised by Ahlcon Public School Mayur Vihar",
+    level: "Interschool",
+    year: "2021",
+    prize: "Winners",
+  },
+  {
+    competition:
+      "Robocon event of Incognito 2021, Somerville School Noida Annual Tech Fest",
+    level: "Interschool",
+    year: "2021",
+    prize: "Winners",
+  },
+  {
+    competition:
+      "Robotix event of Createch 3.0, Ryan International School Vasant Kunj Annual Tech Symposium",
+    level: "Interschool",
+    year: "2021",
+    prize: "Winners",
+  },
+  {
+    competition:
+      "Scinapse event of CodeWars, DPS Vasant Kunj Annual Tech Symposium",
+    level: "Interschool",
+    year: "2021",
+    prize: "Runners Up",
+  },
+  {
+    competition:
+      "Robotics event of CodeWars, DPS Vasant Kunj Annual Tech Symposium",
+    level: "Interschool",
+    year: "2021",
+    prize: "Second Runners Up",
+  },
+  {
+    competition: "Answer and Assemble event of DPS Dwarka Annual Fest",
+    level: "Interschool",
+    year: "2021",
+    prize: "Runners Up",
+  },
+  {
+    competition: "Robothon event of DPS Dwarka Annual Fest",
+    level: "Interschool",
+    year: "2021",
+    prize: "Second Runners Up",
+  },
+  {
+    competition: "Circuitron event of DPS Dwarka Annual Fest",
+    level: "Interschool",
+    year: "2021",
+    prize: "Second Runners Up",
+  },
+  {
+    competition: "Real World Design Challenge-National Round",
+    level: "National",
+    year: "2021",
+    prize: "Runners Up",
+  },
+  {
+    competition: "DeepN  event of Elan & nVision by IITH",
+    level: "International",
+    year: "2021",
+    prize: "Finalist",
+  },
+  {
+    competition: "AutoDesk App Design event of Techkriti'21 IITK",
+    level: "International",
+    year: "2021",
+    prize: "Second Runners Up",
+  },
+  {
+    competition: "Brain-Wreck event of Equilibrium'21, held at MITAOE, Pune",
+    level: "National",
+    year: "2021",
+    prize: "Second Runners Up",
+  },
+  {
+    competition: "RoboCad event of TechKriti'21, held at IIT Kanpur",
+    level: "International",
+    year: "2021",
+    prize: "Runners Up",
+  },
+  {
+    competition:
+      "Techkriti Innovation Challenge event of TechKriti'21 (Project VARSY), held at IIT Kanpur",
+    level: "International",
+    year: "2021",
+    prize: "Finalist",
+  },
+  {
+    competition:
+      "Techkriti Innovation Challenge event of TechKriti'21 (Project GARUD 1.0), held at IIT Kanpur",
+    level: "International",
+    year: "2021",
+    prize: "Finalist",
+  },
+];
