7f5fae1c4441ff6339d849146915cfff2ea9382a
IDoEverything <65034167+IDoEverything@users.noreply.github.com>
2022-04-30 20:09:59 +0530

    update achievements

--- data/achievements.ts
+++ data/achievements.ts
@@ -102,18 +102,648 @@ export const achievements = [
     year: "2021",
     prize: "Finalist",
   },
-    {
+  {
     competition:
       "Ideathon Challenge event of National Road Safety Month by the Government of India, held virtually",
     level: "National",
     year: "2020",
     prize: "Token of appreciation",
   },
-   {
+  {
     competition:
       "P.R.A.I.S.E event of Elysium by IIITD, held virtually",
     level: "National",
     year: "2020",
     prize: "Winners",
   },
+  {
+    competition: "\"Technovanza\" event of \"ATAL Tinkering Fest\" by Manav Sthali School",
+    level: "Interschool",
+    year: "2020",
+    prize: "Second runners up\n"
+  },
+  {
+    competition: "\u201cArtificial Expedition\u201d event of Technovision, held at Ahlcon Public School",
+    level: "Interschool",
+    year: "2020",
+    prize: "Runners-up\n"
+  },
+  {
+    competition: "\u201cElectronigma\u201d event of INNERVE 2020 held at Indira Gandhi Delhi Technical University for Women",
+    level: "National",
+    year: "2020",
+    prize: "Winners\n"
+  },
+  {
+    competition: "\u2018Time Challenge\u2019 event of \u2018Quark\u2019 held at BITS Pilani, Goa",
+    level: "National",
+    year: "2020",
+    prize: "Winners\n"
+  },
+  {
+    competition: "\u2018Circuit Designing\u2019 event of \u2018MINETX\u2019 held at Mother\u2019s International",
+    level: "Interschool",
+    year: "2020",
+    prize: "Winners\n"
+  },
+  {
+    competition: "\u2018tinkerCAD\u2019 event of \u2018Root\u2019 held at Ryan International School, Vasant Kunj",
+    level: "Interschool",
+    year: "2020",
+    prize: "Runners Up\n"
+  },
+  {
+    competition: "\u2018Virtual Robotics\u2019 event of \u2018Robotronics\u2019 held at Amity International",
+    level: "Interschool",
+    year: "2020",
+    prize: "Runners Up\n"
+  },
+  {
+    competition: "Bal Bharti School, Gangaram [Line Follower]",
+    level: "Interschool",
+    year: "2019",
+    prize: "Winners\n"
+  },
+  {
+    competition: "\u2018Arduino\u2019 event of \u2018Ncrypt\u2019 held at DPS Sushant Lok",
+    level: "Interschool",
+    year: "2019",
+    prize: "Winners\n"
+  },
+  {
+    competition: "\u2018Roborace\u2019 event of \u2018Conclave\u2019 held at KH Model School",
+    level: "Interschool",
+    year: "2019",
+    prize: "Winners\n"
+  },
+  {
+    competition: "\u2018Robowars\u2019 event of \u2018Innovation\u2019 held at DPS International Saket",
+    level: "Interschool",
+    year: "2019",
+    prize: "Runners Up\n"
+  },
+  {
+    competition: "\u2018RoboRace\u2019 event held at Amritya Sangam, Saket",
+    level: "Interschool",
+    year: "2019",
+    prize: "Runners Up\n"
+  },
+  {
+    competition: "\u2018Robosoccer\u2019 event of \u2018Haywire\u2019 held at Mount Carmel, Dwarka",
+    level: "Interschool",
+    year: "2019",
+    prize: "Second Runners up\n"
+  },
+  {
+    competition: "Roborace' event of CODE held at Apeejay Public School, Noida",
+    level: "Interschool",
+    year: "2019",
+    prize: "Second Runners up\n"
+  },
+  {
+    competition: "RoboWars' event of 'Byte.IT' held by Bal Bharti, Pitampura ",
+    level: "Interschool",
+    year: "2019",
+    prize: "Winners\n"
+  },
+  {
+    competition: "Drone Championship' event of 'Vrindavan Yojna' held at Allenhouse Public School",
+    level: "National",
+    year: "2019",
+    prize: "Winners\n"
+  },
+  {
+    competition: "ATL Line Following' event at Bal Bharti Public School, Gangaram",
+    level: "Interschool",
+    year: "2019",
+    prize: "Winners\n"
+  },
+  {
+    competition: "Line Follower' event at Kailash Confluence",
+    level: "Interschool",
+    year: "2018",
+    prize: "Winners\n"
+  },
+  {
+    competition: "Roborace\u2019 held at Kailash Confluence",
+    level: "Interschool",
+    year: "2018",
+    prize: "Winners\n"
+  },
+  {
+    competition: "\u2018Fake\u2019 event of \u2018Tryst\u2019 held at IIT, Delhi",
+    level: "International",
+    year: "2018",
+    prize: "Winners\n"
+  },
+  {
+    competition: "\u2018Mind Maze\u2019 event of \u2018Tryst\u2019 held at IIT, Delhi",
+    level: "International",
+    year: "2018",
+    prize: "Runners Up\n"
+  },
+  {
+    competition: "\u2018Paper Presentation\u2019 event of \u2018Tryst\u2019 held at IIT, Delhi",
+    level: "National",
+    year: "2018",
+    prize: "Runners Up\n"
+  },
+  {
+    competition: "\u2018RoboWars\u2019 event of \u2018nCrypt\u2019 held at Delhi Public School, Sushant Lok",
+    level: "Interschool",
+    year: "2018",
+    prize: "Winners\n"
+  },
+  {
+    competition: "\u2018RoboWars\u2019 event of \u2018DynamiX\u2019 held at Ramjas School, R.K.Puram",
+    level: "Interschool",
+    year: "2018",
+    prize: "Winners\n"
+  },
+  {
+    competition: "\u2018Robotics\u2019 event of \u2018MINET\u2019 held at Mother\u2019s International School",
+    level: "Interschool",
+    year: "2018",
+    prize: "Second Runners Up\n"
+  },
+  {
+    competition: "\u2018Sumo Warfare\u2019 event of \u2018Byte.IT\u2019 held at Bal Bharti Public School, Pitampura",
+    level: "Interschool",
+    year: "2018",
+    prize: "\n"
+  },
+  {
+    competition: "\u2018Robocon\u2019 event of \u2018ESYA\u2019 held at IIIT Delhi",
+    level: "National",
+    year: "2018",
+    prize: "Runners up\n"
+  },
+  {
+    competition: "\u2018Robomaze\u2019 event of ESYA held at IIIT Delhi",
+    level: "National",
+    year: "2018",
+    prize: "Winner\n"
+  },
+  {
+    competition: "\u2018Fastest Line Following Challenge\u2019 of Technoxian held at Thyagraj Stadium, New Delhi",
+    level: "International",
+    year: "2018",
+    prize: "Second Runners Up\n"
+  },
+  {
+    competition: "\u2018Robowars\u2019 of Technoxian held at Thyagraj Stadium, New Delhi",
+    level: "International",
+    year: "2018",
+    prize: "Runners Up\n"
+  },
+  {
+    competition: "\u2018Circuitron\u2019 event of \u2018Ignite and Axiom\u2019 held at DPS Dwarka",
+    level: "Interschool",
+    year: "2018",
+    prize: "Winners\n"
+  },
+  {
+    competition: "\u2018RoboCon\u2019 event of \u2018ESYA\u2019 held at IIIT, Delhi",
+    level: "National",
+    year: "2017",
+    prize: "Winners\n"
+  },
+  {
+    competition: "\u2018Big Bang\u2019 event of \u2018ESYA\u2019 held at IIIT, Delhi",
+    level: "National",
+    year: "2017",
+    prize: "Winners\n"
+  },
+  {
+    competition: "\u2018Water Rocket\u2019 held at Technoxian",
+    level: "International",
+    year: "2017",
+    prize: "Runners Up\n"
+  },
+  {
+    competition: "\u2018Line Follower Bot Challenge\u2019 event of \u2018Robotsavam\u2019 held at Amrita University, Amritapuri Campus",
+    level: "National",
+    year: "2017",
+    prize: "Second Runners Up\n"
+  },
+  {
+    competition: "\u2018Maze Solving Bot Challenge\u2019 event of \u2018Robotsavam\u2019 held at Amrita University, Amritapuri Campus",
+    level: "National",
+    year: "2017",
+    prize: "Second Runners Up\n"
+  },
+  {
+    competition: "\u2018Fastest Line Follower\u2019 held at Technoxian",
+    level: "International",
+    year: "2017",
+    prize: "Second Runners Up\n"
+  },
+  {
+    competition: "\u2018RoboMaze\u2019 event of \u2018Incognito\u2019 held at Somerville School, Noida",
+    level: "Inter-School",
+    year: "2017",
+    prize: "Winners\n"
+  },
+  {
+    competition: "\u2018Robotics\u2019 event at \u2018Access\u2019 held at Modern School, Barakhamba Road",
+    level: "Inter-School",
+    year: "2017",
+    prize: "Winners\n"
+  },
+  {
+    competition: "\u2018RoboWars\u2019 event of \u2018Incognito\u2019 held at Somerville School, Noida",
+    level: "Inter-School",
+    year: "2017",
+    prize: "Winners\n"
+  },
+  {
+    competition: "\u2018Trackathon\u2019 event of \u2018Digilogous 4.0\u2019 held at Rukmini Devi Public School",
+    level: "Inter-School",
+    year: "2017",
+    prize: "Runners Up\n"
+  },
+  {
+    competition: "\u2018Robosaga\u2019 event of \u2018Hash 5.0\u2019 held at Darbari Lal Dav Model School",
+    level: "Inter-School",
+    year: "2017",
+    prize: "Runner Up\n"
+  },
+  {
+    competition: "\u2018RoboWars\u2019 event of \u2018Silico Battles\u2019 held at Ahlcon Public School, Mayur VIhar",
+    level: "Inter-School",
+    year: "2017",
+    prize: "Runners Up\n"
+  },
+  {
+    competition: "\u2018RoboWars\u2019 event of \u2018nCrypt\u2019 held at DPS Sushant Lok",
+    level: "Inter-School",
+    year: "2017",
+    prize: "Runners Up\n"
+  },
+  {
+    competition: "\u2018RoboOlympix\u2019 event of \u2018DynamiX\u2019 held at Ramjas Public School, R.K.Puram",
+    level: "Inter-School",
+    year: "2017",
+    prize: "Second Runners Up\n"
+  },
+  {
+    competition: "\u2018RoboWars\u2019 event of \u2018Wartex 3.0\u2019 held at Shaheed Rajpal DAV Public School",
+    level: "Inter-School",
+    year: "2017",
+    prize: "Second Runners Up\n"
+  },
+  {
+    competition: "Rescue' event of RCJ North Zone held at Shiv Nadar School Noida",
+    level: "Regional (Delhi)",
+    year: "2016",
+    prize: "Best Teamwork\n"
+  },
+  {
+    competition: "BOT.AI' event of Access held at Modern School Barakhamba Road ",
+    level: "Interschool",
+    year: "2016",
+    prize: "Runners Up\n"
+  },
+  {
+    competition: "Auto-Gizmo event of Cosmic 350 held at Sanskriti School",
+    level: "Interschool",
+    year: "2016",
+    prize: "Winners\n"
+  },
+  {
+    competition: "Football event of Wartex 2.0 held at SRDAV school",
+    level: "Interschool ",
+    year: "2016",
+    prize: "Runners Up\n"
+  },
+  {
+    competition: "Ramp-On event of Wartex 2.0 held at SRDAV school",
+    level: "Interschool ",
+    year: "2016",
+    prize: "Winners \n"
+  },
+  {
+    competition: "Comlumban Wars of Columban Fest held at St. Columbus",
+    level: "Interschool",
+    year: "2016",
+    prize: "Winners\n"
+  },
+  {
+    competition: "Robowars event of Silico Battles held at Ahlcon Public School",
+    level: "Interschool",
+    year: "2016",
+    prize: "Winners\n"
+  },
+  {
+    competition: "Robosaga event of HASH 4.0 held at DL DAV",
+    level: "Interschool ",
+    year: "2016",
+    prize: "Winners\n"
+  },
+  {
+    competition: "nRoboxter event of nCRYT held at DPS Sushant Lok",
+    level: "Interschool ",
+    year: "2016",
+    prize: "Runners Up\n"
+  },
+  {
+    competition: "Robochase event of B.O.T held at Mayoor School Noida",
+    level: "Interschool ",
+    year: "2016",
+    prize: "Winners\n"
+  },
+  {
+    competition: "Rododex event at DPS Dwarka",
+    level: "Interschool ",
+    year: "2016",
+    prize: "Winners\n"
+  },
+  {
+    competition: "Robo-Olympix event of DynamiX held at Ramjas, RK Puram",
+    level: "Interschool ",
+    year: "2016",
+    prize: "Winners\n"
+  },
+  {
+    competition: "CONRAD Spirit of Innovation Challenge ",
+    level: "International",
+    year: "2015",
+    prize: "Global Finalists\n"
+  },
+  {
+    competition: "Robowars event of Silico Battles held at Ahlcon Public School",
+    level: "Interschool ",
+    year: "2015",
+    prize: "Runners Up\n"
+  },
+  {
+    competition: "Dynamix event of Ramjas, RK Puram",
+    level: "Interschool",
+    year: "2015",
+    prize: "Winners\n"
+  },
+  {
+    competition: "Intel Make-a-thon, by Intel held at Ramjas, RK Puram",
+    level: "National",
+    year: "2015",
+    prize: "Winners\n"
+  },
+  {
+    competition: "Circuitrix Junior event of ESYA held at IIIT Delhi",
+    level: "National ",
+    year: "2015",
+    prize: "Winners\n"
+  },
+  {
+    competition: "Rescue' event of RCJ North Zone held at Shiv Nadar School Noida",
+    level: "National",
+    year: "2015",
+    prize: "Finalists\n"
+  },
+  {
+    competition: "Rescue' event of RCJ North Zone held at Shiv Nadar School Noida",
+    level: "National",
+    year: "2014",
+    prize: "Second Runners Up\n"
+  },
+  {
+    competition: "Junior Robotics event of CORE held at DPS Dwarka",
+    level: "Interschool",
+    year: "2014",
+    prize: "Second Runners Up\n"
+  },
+  {
+    competition: "Senior Robotics event of CORE held at DPS Dwarka",
+    level: "Interschool",
+    year: "2014",
+    prize: "Runners Up\n"
+  },
+  {
+    competition: "RoboWars event of CORE held at DPS Dwarka",
+    level: "Interschool",
+    year: "2014",
+    prize: "Winners\n"
+  },
+  {
+    competition: "RoboWars event of CODE'14 held at APJ Noida",
+    level: "Interschool",
+    year: "2014",
+    prize: "Runners Up\n"
+  },
+  {
+    competition: "Crossroads event of Tech 350 held at Sanskriti School",
+    level: "Interschool",
+    year: "2014",
+    prize: "Winners\n"
+  },
+  {
+    competition: "Platform101 event of Techniche held at IIT Guwahati",
+    level: "International",
+    year: "2013",
+    prize: "Third Runners Up\n"
+  },
+  {
+    competition: "\u201cRobo-rash\u201d event of \u201cInterface-2013\u201d held at St. Xaviers",
+    level: "Interschool",
+    year: "2013",
+    prize: "Runners Up\n"
+  },
+  {
+    competition: "Robothon event of GRID 2013 held at Birla Vidya Niketan",
+    level: "Interschool ",
+    year: "2013",
+    prize: "Second Runners Up\n"
+  },
+  {
+    competition: "RoboWars event of CODE held at APJ Noida",
+    level: "Interschool ",
+    year: "2013",
+    prize: "Winners and Runners Up\n"
+  },
+  {
+    competition: "\u201cBattle of Titans\u201d held at Mayur School Noida",
+    level: "Interschool ",
+    year: "2013",
+    prize: "Runners Up\n"
+  },
+  {
+    competition: "Robowars event of CODEWARS held at DPS Vasant Kunj",
+    level: "Interschool ",
+    year: "2013",
+    prize: "Winners \n"
+  },
+  {
+    competition: "RoboDex event held at DPS Dwarka",
+    level: "Interschool ",
+    year: "2013",
+    prize: "Winners and 2nd Runners Up\n"
+  },
+  {
+    competition: "Technothlon event of Techniche held at IIT Guwahati",
+    level: "International",
+    year: "2011",
+    prize: "Winners and 2nd Runners Up\n"
+  },
+  {
+    competition: "B.O.T-2011 held at Mayoor School, Noida",
+    level: "Interschool",
+    year: "2011",
+    prize: "Second Runners Up\n"
+  },
+  {
+    competition: "RoboDex event held at DPS Dwarka",
+    level: "Interschool ",
+    year: "2011",
+    prize: "Winners and 2nd Runners Up\n"
+  },
+  {
+    competition: "RoboWars event of CODE held at APJ Noida",
+    level: "Interschool ",
+    year: "2010",
+    prize: "Winners \n"
+  },
+  {
+    competition: "Robotics event of ACCESS held at Modern School Barakhamba",
+    level: "Interschool ",
+    year: "2010",
+    prize: "Winners \n"
+  },
+  {
+    competition: "Robotronics event of technolympics held at Cambridge school",
+    level: "Interschool ",
+    year: "2010",
+    prize: "Winners \n"
+  },
+  {
+    competition: "IRIS National Fair 2009",
+    level: "National",
+    year: "2009",
+    prize: "Special Award\n"
+  },
+  {
+    competition: "Code@trix event held at Tagore International, Vasant Vihar",
+    level: "Interschool ",
+    year: "2009",
+    prize: "Runners Up\n"
+  },
+  {
+    competition: "CBSE Science Exhibitiion held at Springdales school Pusa Road",
+    level: "National",
+    year: "2008",
+    prize: "Winners\n"
+  },
+  {
+    competition: "Roborage event of Bro. Aloysius held at St. Mary School",
+    level: "Interschool",
+    year: "2008",
+    prize: "Best Concept\n"
+  },
+  {
+    competition: "Dr. A. K. Saha Young Scientists Experimental Project held at Springdales School, Dhaula Kuan",
+    level: "Interschool",
+    year: "2008",
+    prize: "Runners Up\n"
+  },
+  {
+    competition: "Shri Khoj Science Competition held at Shri Ram School",
+    level: "Interschool",
+    year: "2008",
+    prize: "Winners\n"
+  },
+  {
+    competition: "Different Strokes 2K7\u201d event held at Jaspal Kaur Public School, Shalimar Bagh",
+    level: "Interschool",
+    year: "2007",
+    prize: "Second Runners Up\n"
+  },
+  {
+    competition: "Robotics event of ACCESS held at Modern School Barakhamba",
+    level: "Interschool",
+    year: "2007",
+    prize: "Winners\n"
+  },
+  {
+    competition: "Radix event of Troika 2008 held at Delhi College of Engineering",
+    level: "Interschool",
+    year: "2007",
+    prize: "Second Runners Up\n"
+  },
+  {
+    competition: "VOID event of Techfest - 2006 held at IIT Bombay",
+    level: "International",
+    year: "2006",
+    prize: "Finalists\n"
+  },
+  {
+    competition: "Techniche 2005 held at IIT Guwahati",
+    level: "International",
+    year: "2005",
+    prize: "Finalists\n"
+  },
+  {
+    competition: "GRIP event of Techfest - 2006 held at IIT Bombay",
+    level: "International",
+    year: "2005",
+    prize: "Judges Award\n"
+  },
+  {
+    competition: "Innova 06 event held at Delhi College of Engineering",
+    level: "National",
+    year: "2005",
+    prize: "Second Runners Up\n"
+  },
+  {
+    competition: "Yantriki event held at IIT Bombay",
+    level: "International",
+    year: "2005",
+    prize: "Runners Up\n"
+  },
+  {
+    competition: "Asimo Science Olympiad by Honda held at National Science Centre",
+    level: "National",
+    year: "2004",
+    prize: "Winners\n"
+  },
+  {
+    competition: "Robomania event of Tryst held at IIT Delhi",
+    level: "International",
+    year: "2004",
+    prize: "Winners\n"
+  },
+  {
+    competition: "School Bag event of Techkriti held at IIT Kanpur",
+    level: "International",
+    year: "2004",
+    prize: "Overall Winner\n"
+  },
+  {
+    competition: "Model Making event by Honda held at National Science Centre",
+    level: "National",
+    year: "2003",
+    prize: "Second Runners Up\n"
+  },
+  {
+    competition: "Intel Science Talent discovery Fair 2003-2004 (ISTDF) held at IIIT Hyderabad",
+    level: "National",
+    year: "2003",
+    prize: "Special Award\n"
+  },
+  {
+    competition: "Robomania event of Tryst held at IIT Delhi",
+    level: "International",
+    year: "2003",
+    prize: "Winners\n"
+  },
+  {
+    competition: "Yantriki event held at IIT Bombay",
+    level: "International",
+    year: "2003",
+    prize: "Special Mention\n"
+  },
+  {
+    competition: "Pushover event held at Delhi College of Engineering",
+    level: "National ",
+    year: "2002",
+    prize: "Second Runners Up"
+  }
 ];
