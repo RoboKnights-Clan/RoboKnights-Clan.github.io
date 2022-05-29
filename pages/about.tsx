e9a080ac0ded5b29df259dab70b2127ce7032f63
GarvJain03 <garvj03@gmail.com>
2022-05-29 21:02:43 +0530

    remove placeholder text

--- pages/about.tsx
+++ pages/about.tsx
@@ -24,8 +24,11 @@ const AboutPage = () => {
             participate and win competitions at regional, national and
             international levels consistently. The club also boasts an extensive
             alumni network at numerous MNCs and top-universities all around the
-            world. Our motto, <span className="font-sanssm">"Curiosity is the key to Creativity"</span> speaks
-            volumes about our objective and love for technology.
+            world. Our motto,{" "}
+            <span className="font-sanssm">
+              "Curiosity is the key to Creativity"
+            </span>{" "}
+            speaks volumes about our objective and love for technology.
           </p>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
@@ -38,16 +41,18 @@ const AboutPage = () => {
       </div>
       <div className="container mx-auto md:px-24 flex px-5 py-12 flex-col items-center">
         {about.map((item, index) => (
-          <div className="border-2 border-black dark:border-gray-400 mb-4 rounded-md" key={index}>
+          <div
+            className="border-2 border-black dark:border-gray-400 mb-4 rounded-md w-full"
+            key={index}
+          >
             <div className="container mx-auto md:px-24 flex px-5 py-12 md:flex-row flex-col items-center">
               <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                 <h1 className=" sm:text-2xl text-xl mb-2 font-bold text-dark dark:text-gray-e9">
                   {item.name}
                 </h1>
-                <h1 className=" sm:text-xl text-lg mb-4 font-medium text-gray-cb dark:text-gray-300 dark:text-grey">
+                <h1 className=" sm:text-xl text-lg mb-4 font-medium text-gray-700 dark:text-gray-300 dark:text-grey">
                   {item.role}
                 </h1>
-                <p className="mb-8 leading-relaxed text-justify text-dark dark:text-gray-e9 font-sans">{item.desc}</p>
               </div>
               <div className="w-72">
                 <img
