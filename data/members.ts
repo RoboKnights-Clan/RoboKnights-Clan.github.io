d9774a0e6b2e5f8c316ffd17e6f0e9242fab73ff
arhaan2010 <76624316+arhaan2010@users.noreply.github.com>
2024-11-19 15:25:28 +0530

    Update members.ts

--- data/members.ts
+++ data/members.ts
@@ -1,86 +1,45 @@
-
-
 export const members = [
- 
   {
     year: "Class 12",
     members: [
-       {
+      {
         name: "Twamadi Sar",
         role: "President",
         src: "/images/members/Mishaa.jpg",
         socials: [
-          {
-            type: "instagram",
-            url: "https://www.instagram.com/twamadi/",
-          },
-          {
-            type: "github",
-            url: "https://github.com",
-          },
-          {
-            type: "linkedin",
-            url: "https://www.linkedin.com/in/twamadisar/",
-          },
+          { type: "instagram", url: "https://www.instagram.com/twamadi/" },
+          { type: "github", url: "https://github.com" },
+          { type: "linkedin", url: "https://www.linkedin.com/in/twamadisar/" },
         ],
       },
-     {
+      {
         name: "Shiven Kanodia",
         role: "Director",
         src: "/images/members/Mishaa.jpg",
         socials: [
-          {
-            type: "instagram",
-            url: "https://www.instagram.com",
-          },
-          {
-            type: "github",
-            url: "https://github.com",
-          },
-          {
-            type: "linkedin",
-            url: "https://www.linkedin.com/in/",
-          },
+          { type: "instagram", url: "https://www.instagram.com" },
+          { type: "github", url: "https://github.com" },
+          { type: "linkedin", url: "https://www.linkedin.com/in/" },
         ],
       },
-   {
+      {
         name: "Adhyann Singal",
         role: "Core Member",
         src: "/images/members/Adhyann.png",
-         socials: [
-          {
-            type: "instagram",
-            url: "https://www.instagram.com/",
-          },
-          {
-            type: "github",
-            url: "https://github.com",
-          },
-          {
-            type: "linkedin",
-            url: "https://www.linkedin.com/in",
-          },
+        socials: [
+          { type: "instagram", url: "https://www.instagram.com/" },
+          { type: "github", url: "https://github.com" },
+          { type: "linkedin", url: "https://www.linkedin.com/in" },
         ],
       },
-      
-
       {
         name: "S. Shrikirti",
         role: "Senior Member",
         src: "/images/members/Shrikirti.png",
-            socials: [
-          {
-            type: "instagram",
-            url: "https://www.instagram.com/",
-          },
-          {
-            type: "github",
-            url: "https://github.com",
-          },
-          {
-            type: "linkedin",
-            url: "https://www.linkedin.com/in",
-          },
+        socials: [
+          { type: "instagram", url: "https://www.instagram.com/" },
+          { type: "github", url: "https://github.com" },
+          { type: "linkedin", url: "https://www.linkedin.com/in" },
         ],
       },
       {
@@ -88,76 +47,39 @@ export const members = [
         role: "Member",
         src: "/images/members/Mishaa.jpg",
         socials: [
-          {
-            type: "instagram",
-            url: "https://www.instagram.com/mishaa1109/",
-          },
-          {
-            type: "github",
-            url: "https://github.com/Mishaa1109",
-          },
-          {
-            type: "linkedin",
-            url: "https://www.linkedin.com/in/mishaa-kalra-513772222/",
-          },
+          { type: "instagram", url: "https://www.instagram.com/mishaa1109/" },
+          { type: "github", url: "https://github.com/Mishaa1109" },
+          { type: "linkedin", url: "https://www.linkedin.com/in/mishaa-kalra-513772222/" },
         ],
       },
-      
       {
         name: "Mannat Kaur",
         role: "Member",
         src: "/images/members/Mannat.png",
-            socials: [
-          {
-            type: "instagram",
-            url: "https://www.instagram.com/",
-          },
-          {
-            type: "github",
-            url: "https://github.com",
-          },
-          {
-            type: "linkedin",
-            url: "https://www.linkedin.com/in",
-          },
+        socials: [
+          { type: "instagram", url: "https://www.instagram.com/" },
+          { type: "github", url: "https://github.com" },
+          { type: "linkedin", url: "https://www.linkedin.com/in" },
         ],
       },
       {
         name: "Rihit Wadhwa",
         role: "Member",
         src: "/images/members/Rihit.jpg",
-            socials: [
-          {
-            type: "instagram",
-            url: "https://www.instagram.com/",
-          },
-          {
-            type: "github",
-            url: "https://github.com",
-          },
-          {
-            type: "linkedin",
-            url: "https://www.linkedin.com/in",
-          },
+        socials: [
+          { type: "instagram", url: "https://www.instagram.com/" },
+          { type: "github", url: "https://github.com" },
+          { type: "linkedin", url: "https://www.linkedin.com/in" },
         ],
       },
       {
         name: "Rishit Aggarwal",
         role: "Member",
         src: "https://media.discordapp.net/attachments/753327283497992356/1119327685588557965/Rishit_Aggarwal_11-E.JPG?width=472&height=620",
-            socials: [
-          {
-            type: "instagram",
-            url: "https://www.instagram.com/",
-          },
-          {
-            type: "github",
-            url: "https://github.com",
-          },
-          {
-            type: "linkedin",
-            url: "https://www.linkedin.com/in",
-          },
+        socials: [
+          { type: "instagram", url: "https://www.instagram.com/" },
+          { type: "github", url: "https://github.com" },
+          { type: "linkedin", url: "https://www.linkedin.com/in" },
         ],
       },
     ],
@@ -170,52 +92,28 @@ export const members = [
         role: "Member",
         src: "/images/members/Yuvraj.png",
         socials: [
-          {
-            type: "youtube",
-            url: "https://m.youtube.com/channel/UCqr9DHvhR0r-JIoutyNFhxg",
-          },
-          {
-            type: "linkedin",
-            url: "https://www.linkedin.com/in/yuvraj-matta-131764220",
-          },
+          { type: "youtube", url: "https://m.youtube.com/channel/UCqr9DHvhR0r-JIoutyNFhxg" },
+          { type: "linkedin", url: "https://www.linkedin.com/in/yuvraj-matta-131764220" },
         ],
       },
       {
         name: "Shaurya Markanda",
         role: "Member",
         src: "/images/members/Shaurya.png",
-       socials: [
-          {
-            type: "instagram",
-            url: "https://www.instagram.com/",
-          },
-          {
-            type: "github",
-            url: "https://github.com",
-          },
-          {
-            type: "linkedin",
-            url: "https://www.linkedin.com/in",
-          },
+        socials: [
+          { type: "instagram", url: "https://www.instagram.com/" },
+          { type: "github", url: "https://github.com" },
+          { type: "linkedin", url: "https://www.linkedin.com/in" },
         ],
       },
       {
         name: "Karun Sharma",
         role: "Member",
         src: "https://cdn.discordapp.com/attachments/753327283497992356/1103014789057351841/KARUN.jpg",
-       socials: [
-          {
-            type: "instagram",
-            url: "https://www.instagram.com/",
-          },
-          {
-            type: "github",
-            url: "https://github.com",
-          },
-          {
-            type: "linkedin",
-            url: "https://www.linkedin.com/in",
-          },
+        socials: [
+          { type: "instagram", url: "https://www.instagram.com/" },
+          { type: "github", url: "https://github.com" },
+          { type: "linkedin", url: "https://www.linkedin.com/in" },
         ],
       },
     ],
@@ -228,22 +126,9 @@ export const members = [
         role: "Member",
         src: "/images/members/",
         socials: [
-          {
-                socials: [
-          {
-            type: "instagram",
-            url: "https://www.instagram.com/",
-          },
-          {
-            type: "github",
-            url: "https://github.com",
-          },
-          {
-            type: "linkedin",
-            url: "https://www.linkedin.com/in",
-          },
-        ],
-          },
+          { type: "instagram", url: "https://www.instagram.com/" },
+          { type: "github", url: "https://github.com" },
+          { type: "linkedin", url: "https://www.linkedin.com/in" },
         ],
       },
       {
@@ -251,22 +136,9 @@ export const members = [
         role: "Member",
         src: "/images/members/Agastya.jpg",
         socials: [
-          {
-                socials: [
-          {
-            type: "instagram",
-            url: "https://www.instagram.com/",
-          },
-          {
-            type: "github",
-            url: "https://github.com",
-          },
-          {
-            type: "linkedin",
-            url: "https://www.linkedin.com/in",
-          },
-        ],
-          },
+          { type: "instagram", url: "https://www.instagram.com/" },
+          { type: "github", url: "https://github.com" },
+          { type: "linkedin", url: "https://www.linkedin.com/in" },
         ],
       },
     ],
@@ -278,57 +150,30 @@ export const members = [
         name: "Medhansh Pandya",
         role: "Member",
         src: "/images/members/Medhansh.png",
-            socials: [
-          {
-            type: "instagram",
-            url: "https://www.instagram.com/",
-          },
-          {
-            type: "github",
-            url: "https://github.com",
-          },
-          {
-            type: "linkedin",
-            url: "https://www.linkedin.com/in",
-          },
+        socials: [
+          { type: "instagram", url: "https://www.instagram.com/" },
+          { type: "github", url: "https://github.com" },
+          { type: "linkedin", url: "https://www.linkedin.com/in" },
         ],
       },
       {
         name: "Arhaan Sharma",
         role: "Member",
         src: "/images/members/Arhaan.png",
-         socials: [
-          {
-            type: "github",
-            url: "https://github.com/arhaan2010",
-          },
-          {
-            type: "linkedin",
-            url: "https://www.linkedin.com/in/flyby-robotics-90150a27b/",
-          },
-           {
-            type: "instagram",
-            url: "https://www.instagram.com/_arhaan.s_/",
-          },
+        socials: [
+          { type: "github", url: "https://github.com/arhaan2010" },
+          { type: "linkedin", url: "https://www.linkedin.com/in/flyby-robotics-90150a27b/" },
+          { type: "instagram", url: "https://www.instagram.com/_arhaan.s_/" },
         ],
       },
       {
         name: "Adhiraj Jain",
         role: "Member",
         src: "/images/members/Adhiraj.png",
-       socials: [
-          {
-            type: "instagram",
-            url: "https://www.instagram.com/",
-          },
-          {
-            type: "github",
-            url: "https://github.com",
-          },
-          {
-            type: "linkedin",
-            url: "https://www.linkedin.com/in",
-          },
+        socials: [
+          { type: "instagram", url: "https://www.instagram.com/" },
+          { type: "github", url: "https://github.com" },
+          { type: "linkedin", url: "https://www.linkedin.com/in" },
         ],
       },
     ],
