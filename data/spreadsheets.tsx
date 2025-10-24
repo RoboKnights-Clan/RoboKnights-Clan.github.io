c38889fbd220cada5674743bafac9c705660eb39
arhaan2010 <76624316+arhaan2010@users.noreply.github.com>
2025-10-24 22:06:59 +0530

    nvm

--- data/spreadsheets.tsx
+++ data/spreadsheets.tsx
@@ -6,9 +6,9 @@ export const spreadsheets = [
     url: "https://docs.google.com/spreadsheets/d/18M5VLCmC0tczG61m9Zx2OWQXP9jOa_oAfpPBo_BhAi8/edit?gid=0#gid=0",
   },
   {
-    slug: "Nike",
+    slug: "nike",
     name: "nikes list",
-    url: "https://docs.google.com/spreadsheets/d/2defABC456/edithttps://docs.google.com/spreadsheets/d/1056zsk6qjHg8TwSRMsnPlvdkzPY-6hsaekKL1jdF0Tc/edit?gid=638141570#gid=638141570",
+    url: "https://docs.google.com/spreadsheets/d/1056zsk6qjHg8TwSRMsnPlvdkzPY-6hsaekKL1jdF0Tc/edit?usp=sharing",
   },
   {
     slug: "budget",
