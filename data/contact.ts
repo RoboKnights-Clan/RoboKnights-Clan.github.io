eac6f4d8a79e0af070e45a30a1990ffc57fa5847
Arhaan Sharma <76624316+arhaan2010@users.noreply.github.com>
2026-05-04 19:43:49 +0530

    Update contact.ts

--- data/contact.ts
+++ data/contact.ts
@@ -14,12 +14,15 @@ export const contacts: ContactPerson[] = [
 {
     name: "Mr Ajith Kumar",
     role: "Head,RoboKnights",
+    email: "",
     remark: "Teacher In-Charge",
+    
   },
   {
     name: "Mr. Mukesh Kumar",
     role: "Vice Principal",
     remark: "Faculty advisor for RoboKnights and club activities.",
+    email: "",
   },
   {
     name: "RoboKnights",
@@ -31,32 +34,38 @@ export const contacts: ContactPerson[] = [
     name: "Arhaan Sharma",
     role: "Executive",
     remark: "Handles technical guidance and robotics workshops.",
+    email: "",
   },
   {
     name: "Medhansh Tanmay Pandya",
     role: "Executive",
     remark: "For collaborations, parts and mechanical design",
+    email: "",
   },
   {
     name: "Aryamman Ojha",
     role: "Executive",
     remark: "Specialises in Wireless communication and electronics.",
+    email: "",
   },
   
   {
     name: "Adhiraj Jain",
     role: "Executive",
     remark: "Specialises in Arduino and 3D Design.",
+    email: "",
   },
    {
     name: "Naitik Jindal",
     role: "Executive",
     remark: "Specialises in event planning and management.",
+    email: "",
   },
   {
     name: "Kyraan Katyal",
     role: "Executive",
     remark: "For collaborations, parts and mechanical design",
+    email: "",
   },
 
 ];
